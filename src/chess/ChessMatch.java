22da961cd02d4a177d4b8e2406596363efb02736
carlosklucas <carlosklucas@hotmail.com>
2021-09-22 19:27:53 +0100

    possible moves of piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -30,7 +30,6 @@ public class ChessMatch {
 	
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
-		
 		Position target = targetPosition.toPosition();
 		
 		validateSourcePosition(source);
@@ -48,6 +47,9 @@ public class ChessMatch {
 	private void validateSourcePosition(Position position) {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
+		}
+		if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no possible moves for the chosen pieces");
 		}
 	}
 	
